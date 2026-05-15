60d3d88087c8353c80fe848688986874ac8458d1
Muhammad Ajmal Na'im bin Ahmad Radhi <man011@lgb.group>
2026-05-15 11:12:38 +0800

    Add backend CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,92 @@ resource "aws_dynamodb_table" "visitor_count" {
     type = "S"
   }
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "visitor-counter-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = aws_dynamodb_table.visitor_count.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "visitor_counter" {
+  function_name = "visitor-counter"
+  runtime       = "python3.9"
+  handler       = "visitor_counter.lambda_handler"
+
+  role = aws_iam_role.lambda_role.arn
+
+  filename         = "../lambda/function.zip"
+  source_code_hash = filebase64sha256("../lambda/function.zip")
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "visitor-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.visitor_counter.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "visitor_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /visitor"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowInvokeFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
